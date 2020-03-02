e7769601ca4947c27c4564d7caeef69ee9b79eef
kohdz <umair.stem@gmail.com>
2020-03-01 19:27:30 -0500

    UI LayedOut

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
+
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+        </Fragment>
+    );
+  }
+}
+
+export default App;
