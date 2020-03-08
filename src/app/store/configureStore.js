f67a101508a8f817690000b4863aac00398aeae1
kohdz <umair.stem@gmail.com>
2020-03-07 20:13:06 -0500

    Firestore bug fix

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -9,7 +9,7 @@ import firebase from '../config/firebase';
 const rrfConfig = {
   userProfile: 'users',
   attachAuthIsReady: true,
-  useFireStoreForProfile: true,
+  useFirestoreForProfile: true,
   updateProfileOnLogin: false
 };
 
