e16974b50b0d3a35c46a4062ce7cb1999efac39d
kohdz <umair.stem@gmail.com>
2020-03-08 20:21:29 -0400

    react createElement bug fix

--- src/features/events/EventDetails/EventDetailedPage.jsx
+++ src/features/events/EventDetails/EventDetailedPage.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventDetailedHeader from './EventsDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
